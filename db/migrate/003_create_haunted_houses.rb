06609cffa11e3b5175d306b13c06a6e0314af3cc
Erin Stalzer <erinstalzer@gmail.com>
2019-05-29 16:37:44 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -13,6 +13,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.1]
         t.datetime :opening_date
         t.datetime :closing_date
         t.string :description
+        t.timestamps
       end
     end
 
