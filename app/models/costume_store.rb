dacef9e1addbd8e20962bb07a30efbb6a24b122a
Erin Stalzer <erinstalzer@gmail.com>
2019-05-29 16:31:19 +0000

    Automatically backed up by Learn

--- app/models/costume_store.rb
+++ app/models/costume_store.rb
@@ -1,4 +1,6 @@
 # Create your CostumeStore class here
 class CostumeStore
 
+  attr_accessor :name, :location, :costume_inventory, :num_of_employees, :still_in_business, :opening_time, :closing_time
+
 end
