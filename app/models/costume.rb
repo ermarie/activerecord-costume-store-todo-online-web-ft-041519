17506015665fe5c81d9d3ecd916d19766533d9b7
Erin Stalzer <erinstalzer@gmail.com>
2019-05-29 16:35:24 +0000

    Automatically backed up by Learn

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -2,7 +2,7 @@
 # It should inherit from ActiveRecord::Base
 class Costume < ActiveRecord::Base
 
-  attr_accessor :name, :price, :size, :imageurl
+  # attr_accessor :name, :price, :size, :imageurl
 
 
 
