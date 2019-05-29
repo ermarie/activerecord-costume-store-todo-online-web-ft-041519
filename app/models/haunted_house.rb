740d598a98e619e30e4aa4dc1891f622317fabca
Erin Stalzer <erinstalzer@gmail.com>
2019-05-29 16:36:04 +0000

    Automatically backed up by Learn

--- app/models/haunted_house.rb
+++ app/models/haunted_house.rb
@@ -1,6 +1,5 @@
 # Create your HauntedHouse class here
 class HauntedHouse < ActiveRecord::Base
 
-  attr_accessor :name, :location, :theme, :price, :family_friendly, :opening_date, :closing_date, :description
 
 end
